ff895215dfa8742b2009eed10d1bd119dba60150
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2022-06-30 20:36:54 +0800

    Update main17.py

--- python-202206/main17.py
+++ python-202206/main17.py
@@ -4,13 +4,17 @@ import requests
 token = ""
 url = "https://notify-api.line.me/api/notify"
 msg = input('請輸入要發送的文字訊息: ')
+pid = int(input('請輸入貼圖 package id: '))
+id = int(input('請輸入貼圖 id: '))
 
 headers = {
     "Authorization": "Bearer " + token
 }
 
 payload = {
-    "message": msg
+    "message": msg,
+    "stickerPackageId": pid,
+    "stickerId": id
 }
 
 resp = requests.post(url, headers=headers, params=payload)
