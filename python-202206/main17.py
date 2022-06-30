7470985d5d54253cc8d9ba191adb0946f1912d51
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2022-06-30 20:11:17 +0800

    Update main17.py

--- python-202206/main17.py
+++ python-202206/main17.py
@@ -1,7 +1,7 @@
 import requests
 # LineNotify 通知
 # https://notify-bot.line.me/zh_TW/
-token = "FUKeTT7kFNd9myOgiA0theirE4Cxn2R4FSu3ZfDE2T4"
+token = ""
 url = "https://notify-api.line.me/api/notify"
 msg = input('請輸入要發送的文字訊息: ')
 
